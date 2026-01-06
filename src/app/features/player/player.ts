030cd4f8ad636cd4d4a7ea43849cd37ee0e860f2
Coderyounes <younesanas2021@gmail.com>
2026-01-06 15:28:59 +0100

    integrate AudioPlayerService and DurationPipe into Player component, add playback controls and state bindings

--- src/app/features/player/player.ts
+++ src/app/features/player/player.ts
@@ -1,11 +1,46 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {AudioPlayerService} from '../../core/services/audio-player.service';
+import {DurationPipe} from '../../shared/pipes/duration.pipe';
 
 @Component({
   selector: 'app-player',
-  imports: [],
+  imports: [
+    DurationPipe
+  ],
   templateUrl: './player.html',
   styleUrl: './player.css',
 })
 export class Player {
+  playerService = inject(AudioPlayerService);
 
+  currentTrack = this.playerService.currentTrack;
+  isPlaying = this.playerService.isPlaying;
+  currentTime = this.playerService.currentTime;
+  duration = this.playerService.duration;
+
+  get volume() { return this.playerService.state().volume; }
+
+
+
+
+  togglePlay() {
+    this.playerService.togglePlay()
+  }
+
+  onSeek(event: Event) {
+    const input = event.target as HTMLInputElement;
+    this.playerService.seekTo(Number(input.value));
+  }
+
+  onVolumeChange(event: Event) {
+    const input = event.target as HTMLInputElement;
+    this.playerService.setVolume(Number(input.value))
+  }
+
+  next() {
+    console.log("Next Track");
+  }
+  previous() {
+    console.log("Previous Track");
+  }
 }
