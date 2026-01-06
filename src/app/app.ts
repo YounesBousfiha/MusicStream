31e1ed19890d0157dd708133b23c06f889cc5978
Coderyounes <younesanas2021@gmail.com>
2026-01-06 14:11:13 +0100

    add Navbar and Player components to app module for enhanced layout and functionality

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Navbar} from './core/layout/navbar/navbar';
+import {Player} from './features/player/player';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navbar, Player],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
