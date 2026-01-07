1ce1c54ea028a0349a20e1aad92b737654e9d283
Coderyounes <younesanas2021@gmail.com>
2026-01-07 11:07:35 +0100

    add form modules, pipes, and services to TrackDetail component, implement track loading, editing, and playback functionality

--- src/app/features/track-detail/track-detail.ts
+++ src/app/features/track-detail/track-detail.ts
@@ -1,14 +1,20 @@
 import {Component, inject, signal} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
-import {Location} from '@angular/common';
+import {DatePipe, DecimalPipe, Location} from '@angular/common';
 import {TrackService} from '../../core/services/track.service';
 import {AudioPlayerService} from '../../core/services/audio-player.service';
-import {FormBuilder, Validators} from '@angular/forms';
+import {FormBuilder, ReactiveFormsModule, Validators} from '@angular/forms';
 import {TrackModel} from '../../core/model/track.model';
+import {DurationPipe} from '../../shared/pipes/duration.pipe';
 
 @Component({
   selector: 'app-track-detail',
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    DurationPipe,
+    DecimalPipe,
+    DatePipe
+  ],
   templateUrl: './track-detail.html',
   styleUrl: './track-detail.css',
 })
@@ -37,8 +43,61 @@ export class TrackDetail {
       if(id) {
         this.loadTrack(id);
       }
-    })
+    });
   }
 
-  loadTrack(id: string) {}
+  loadTrack(id: string) {
+    const foundTrack = this.trackService.getTrackById(id);
+    if(foundTrack) {
+      this.track.set(foundTrack);
+
+      this.editForm.patchValue({
+        title: foundTrack.title,
+        artist: foundTrack.artist,
+        description: foundTrack.description,
+        category: foundTrack.category
+      });
+    }  else {
+      this.router.navigate(['/library']);
+    }
+  }
+
+  toggleEdit() {
+    this.isEditing.update(v => !v);
+    if(!this.isEditing() && this.track()) {
+      this.editForm.patchValue(this.track as any)
+    }
+  }
+
+  async saveChanges() {
+    if (this.editForm.invalid || !this.track()) return;
+
+    this.isSubmitting.set(true);
+
+    const updatedTrack: TrackModel = {
+      ...this.track()!,
+      title: this.editForm.value.title!,
+      artist: this.editForm.value.artist!,
+      description: this.editForm.value.description!,
+      category: this.editForm.value.category!
+    };
+
+    await this.trackService.updateTrack(updatedTrack);
+
+    this.track.set(updatedTrack);
+    this.isEditing.set(false);
+    this.isSubmitting.set(false);
+  }
+
+  async playThis() {
+    if(this.track()) {
+      await this.playerService.playTrack(this.track()!);
+    }
+  }
+
+  goBack() {
+    this.location.back();
+  }
+
+
 }
