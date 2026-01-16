10baec072dae334f3f4005cb42632d111f8469df
Coderyounes <younesanas2021@gmail.com>
2026-01-16 10:16:34 +0100

    add unit tests for DurationPipe to validate time transformation

---
+++ src/app/shared/pipes/duration.pipe.spec.ts
@@ -0,0 +1,23 @@
+import { DurationPipe} from './duration.pipe';
+import {describe, expect} from 'vitest';
+
+
+describe('DurationPipe', () => {
+  const pipe = new DurationPipe();
+
+  it('create an instance', () => {
+    expect(pipe).toBeTruthy();
+  });
+
+  it('should transform seconds to mm:ss format', () => {
+    expect(pipe.transform(65)).toBe('1:05');
+    expect(pipe.transform(125)).toBe('2:05');
+    expect(pipe.transform(9)).toBe('0:09');
+  });
+
+  it('should return 00:00 for 0 or null', () => {
+    expect(pipe.transform(0)).toBe('0:00');
+  })
+})
+
+
